a5060a4728a4d64fc26724527a1c6ea81111123c
Golo Roden <webmaster@goloroden.de>
2011-12-30 22:33:04 +0100

    Added initial version of silkveil.js.

---
+++ app.js
@@ -0,0 +1,56 @@
+var http = require('http');
+var url = require('url');
+
+var mappings = {
+  'goloroden': {
+    action: 'redirect',
+    url: 'http://www.goloroden.de/',
+    type: 'permanent'
+  },
+  'polarbear': {
+    action: 'download',
+    url: 'http://www.goloroden.de/images/Logo.png',
+    fileName: 'PolarBear.png',
+    contentType: 'image/png',
+    forceDownload: false
+  }
+};
+
+var actions = {
+  'download': function (res, mapping) {
+    var contentDisposition = mapping.forceDownload ? 'attachment' : 'inline';
+
+    res.writeHead(200, {
+      'Content-Type': mapping.contentType,
+      'Content-Disposition': contentDisposition + '; filename=' + mapping.fileName + ';'      
+    });
+
+    http.get(url.parse(mapping.url), function (data) {
+      data.pipe(res);
+    });
+  },
+  'error': function (res, mapping) {
+    res.writeHead(mapping.statusCode, {
+      'Content-Type': 'text/html'
+    });
+    res.end(mapping.statusCode + ' ' + mapping.data);
+  },
+  'redirect': function (res, mapping) {
+    var statusCode = mapping.type === 'permanent' ? 301 : 307;
+    res.writeHead(statusCode, {
+      'Location': mapping.url
+    });
+    res.end();
+  }
+};
+
+http.createServer(function (req, res) {
+  var alias = req.url.substring(1);
+  var mapping = mappings[alias] || {
+    action: 'error',
+    statusCode: 404,
+    data: 'File not found'
+  };
+
+  actions[mapping.action](res, mapping);
+}).listen(3000);
