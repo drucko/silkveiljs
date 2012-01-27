3935ee69f210e6561c114b228f05fbd0f20d44bc
Golo Roden <webmaster@goloroden.de>
2012-01-27 15:39:02 +0100

    Added some tests.

---
+++ test/constraints.test.js
@@ -0,0 +1,51 @@
+var assert = require('node-assertthat'),
+    moment = require('moment'),
+    verify = require('../constraints.js').verify;
+
+suite('When verify is called with a mapping', function () {
+  suite('without constraints', function () {
+    test('the mapping is returned.', function () {
+      var mapping = { foo: 'bar' },
+          expected = mapping;
+
+      var actual = verify(mapping);
+
+      assert.that(actual, is.equalTo(expected));
+    });
+  });
+
+  suite('with a validBefore constraint', function () {
+    test('and the given datetime is in the future the mapping is returned.', function () {
+      var mapping = {
+            foo: 'bar',
+            constraints: {
+              validBefore: [ moment(Date.UTC.apply({}, [2081, 1, 17, 23, 59, 59])) ]
+            }
+          },
+          expected = mapping;
+
+      var actual = verify(mapping);
+
+      assert.that(actual, is.equalTo(expected));
+    });
+
+    test('and the given datetime is in the past undefined is returned.', function () {
+    });
+  });
+
+  suite('with a validFrom constraint', function () {
+    test('and the given datetime is in the future undefined is returned.', function () {
+    });
+
+    test('and the given datetime is in the past the mapping is returned.', function () {
+    });
+  });
+
+  suite('with more than one constraint', function () {
+    test('and all constraints are valid the mapping is returned.', function () {
+    });
+
+    test('and at least one constraint is not valid undefined is returned.', function () {
+    });
+  });
+});
